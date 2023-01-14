5fd1659fb974893ddf2979278580d372e2e88dc2
unknown <bni.sergio@gmail.com>
2023-01-14 21:33:49 +0100

    base

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+/* //values and variables
+
+console.log('sergio');
+console.log(23);
+
+let firstName = 'sergio'; //variable escrita en camelCase
+//utilizar variables que sean descriptivas
+console.log(firstName);
+console.log(firstName);
+
+//tarea
+
+const country = 'españita';
+const continent = 'europa';
+const population = 50;
+console.log(country, continent, population);
+
+*/
+//Data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 'hehe');
+console.log(typeof 23);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1990;
+console.log(typeof year);
+console.log(typeof null); //error de javascript
+
+//Let const and var
